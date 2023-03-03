4a8a5a2b94b35d81ad787239808df598b9027749
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-03 10:40:55 +0100

    more communication

--- src/Commands/CreateUser.php
+++ src/Commands/CreateUser.php
@@ -27,13 +27,18 @@ class CreateUser
         echo "The Display Name:\n";
         $display = trim(fgets(STDIN));
         $uuid = Uuid::uuid1();
+        echo "Your user id is $uuid, now generating private and public key pair.\n";
         $private = RSA::createKey($this->env->getInt('SYSTEM_KEY_BYTES'));
+        echo "Private key generated.\n";
         $private->withPassword($master);
+        echo "Master Password set.\n";
         mkdir(__DIR__ . '/../../keys/' . $uuid);
         file_put_contents(__DIR__ . '/../../keys/' . $uuid . '/private', $private->toString('openssl'));
         file_put_contents(__DIR__ . '/../../keys/' . $uuid . '/public', $private->getPublicKey()->toString('openssl'));
+        echo "Keys written to filesystem.\n";
         $this->database
             ->prepare('INSERT INTO accounts (id,display,mail) VALUES (:id,:display,:mail)')
             ->execute([':display' => $display, ':id' => $uuid, ':mail' => $email]);
+        echo "User added to database.\n";
     }
 }
