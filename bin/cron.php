7d8fccac6d253cc2051573da9e5cea1e1cdc2b98
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-03 10:34:36 +0100

    typefix

--- bin/cron.php
+++ bin/cron.php
@@ -12,4 +12,4 @@ require dirname(__DIR__) . '/vendor/autoload.php';
     ->register(new FilesystemLoader(dirname(__DIR__) . '/templates'))
     ->add('cleanup', SessionCleanup::class)
     ->add('register', CreateUser::class)
-    ->run($argv);
+    ->run(...$argv);
