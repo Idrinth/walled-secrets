3cbd1f923e39c802c3eb331438489bdf314858c3
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-04 09:33:11 +0100

    query fixes

--- src/Pages/Search.php
+++ src/Pages/Search.php
@@ -33,10 +33,10 @@ class Search
             header ('Location: /', true, 303);
             return '';
         }
-        $stmt = $this->database->prepare('SELECT public,id FROM logins WHERE public LIKE CONCAT("%",:term,"%") AND `account=:id`');
+        $stmt = $this->database->prepare('SELECT public,id FROM logins WHERE public LIKE CONCAT("%",:term,"%") AND `account`=:id');
         $stmt->execute([':id' => $_SESSION['id'], ':term' => $post['term']]); 
         $logins = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $stmt = $this->database->prepare('SELECT public,id FROM notes WHERE public LIKE CONCAT("%",:term,"%") AND `account=:id`');
+        $stmt = $this->database->prepare('SELECT public,id FROM notes WHERE public LIKE CONCAT("%",:term,"%") AND `account`=:id');
         $stmt->execute([':id' => $_SESSION['id'], ':term' => $post['term']]); 
         $notes = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $this->twig->render('search', [
