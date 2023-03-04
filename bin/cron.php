fac809d9eb286de9bd0ba92983e358aaa85ec41c
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-04 16:14:12 +0100

    add database cleanups

--- bin/cron.php
+++ bin/cron.php
@@ -2,6 +2,7 @@
 
 use De\Idrinth\WalledSecrets\Command;
 use De\Idrinth\WalledSecrets\Commands\CreateUser;
+use De\Idrinth\WalledSecrets\Commands\DataCleanup;
 use De\Idrinth\WalledSecrets\Commands\SessionCleanup;
 use De\Idrinth\WalledSecrets\Services\Database;
 use Twig\Loader\FilesystemLoader;
@@ -11,6 +12,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 (new Command())
     ->register(new Database())
     ->register(new FilesystemLoader(dirname(__DIR__) . '/templates'))
-    ->add('cleanup', SessionCleanup::class)
+    ->add('cleanup-session', SessionCleanup::class)
+    ->add('cleanup-database', DataCleanup::class)
     ->add('register', CreateUser::class)
     ->run(...$argv);
---
+++ src/Commands/DataCleanup.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace De\Idrinth\WalledSecrets\Commands;
+
+use PDO;
+
+class DataCleanup
+{
+    private PDO $database;
+
+    public function __construct(PDO $database)
+    {
+        $this->database = $database;
+    }
+
+    public function run()
+    {
+        $del1 = $this->database->prepare('DELETE FROM logins WHERE folder IN (SELECT folder FROM folders WHERE `type`="Organisation" AND `owner`=:org) AND `account` NOT IN (SELECT `account` FROM memberships WHERE organisation=:org AND `role`<>"Proposed")');
+        $del2 = $this->database->prepare('DELETE FROM notes WHERE folder IN (SELECT folder FROM folders WHERE `type`="Organisation" AND `owner`=:org) AND `account` NOT IN (SELECT `account` FROM memberships WHERE organisation=:org AND `role`<>"Proposed")');
+        $stmt = $this->database->query('SELECT aid FROM organisations');
+        foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $org) {
+            $del1->closeCursor();
+            $del1->execute([':org' => $org['aid']]);
+            $del2->closeCursor();
+            $del2->execute([':org' => $org['aid']]);
+        }
+    }
+}
