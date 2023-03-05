026a4fb7fa1a68fbdbde74f6cd5a887cf4c12ec7
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-05 18:27:11 +0100

    add missing use statement

--- src/API/FirefoxExtension.php
+++ src/API/FirefoxExtension.php
@@ -2,14 +2,15 @@
 
 namespace De\Idrinth\WalledSecrets\API;
 
+use ZipArchive;
+
 class FirefoxExtension
 {
     public function get(): string
     {
         header('Content-Type: application/x-xpinstall', true, 200);
-        #header('Content-Disposition: attachment; filename="idrinths-walled-secrets.xpi"');
         $name = tempnam(sys_get_temp_dir(), 'mozilla-');
-        $zip = new \ZipArchive();
+        $zip = new ZipArchive();
         $zip->open($name, ZipArchive::CREATE|ZipArchive::OVERWRITE);
         $zip->addGlob(dirname(__DIR__, 2) . '/mozilla-addon/*/*', 0, ['remove_path' => dirname(__DIR__, 2) . '/mozilla-addon']);
         $zip->addFromString('manifest.json', str_replace('##HOSTNAME##', $_ENV['SYSTEM_HOSTNAME'], file_exists(dirname(__DIR__, 2) . '/mozilla-addon/manifest.json')));
