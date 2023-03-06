dff1e223b03a34a2e0935d9d8810ff9fd63da61e
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-06 16:55:29 +0100

    make eula,imprint and data protecting configurable via env translate imprint

--- src/Twig.php
+++ src/Twig.php
@@ -20,6 +20,11 @@ class Twig
         $context['website_name'] = $_ENV['SYSTEM_NAME'];
         $context['session_duration'] = $_ENV['SYSTEM_SESSION_DURATION'];
         $context['logged_in'] = isset($_SESSION['id']);
+        $context['contact_email'] = $_ENV['SYSTEM_CONTACT_EMAIL'];
+        $context['contact_name'] = $_ENV['SYSTEM_CONTACT_NAME'];
+        $context['contact_phone'] = $_ENV['SYSTEM_CONTACT_PHONE'];
+        $context['contact_street'] = $_ENV['SYSTEM_CONTACT_STREET'];
+        $context['contact_city'] = $_ENV['SYSTEM_CONTACT_CITY'];
         return $this->twig->render("$template.twig", $context);
     }
 }
