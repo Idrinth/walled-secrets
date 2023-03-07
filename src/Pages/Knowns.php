6bb866caa6d004e56f035097e21ed6e07eece899
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-07 21:40:14 +0100

    add option to send passwords/notes to users

--- src/Pages/Knowns.php
+++ src/Pages/Knowns.php
@@ -4,11 +4,13 @@ namespace De\Idrinth\WalledSecrets\Pages;
 
 use De\Idrinth\WalledSecrets\Services\ENV;
 use De\Idrinth\WalledSecrets\Services\KeyLoader;
+use De\Idrinth\WalledSecrets\Services\ShareWithOrganisation;
 use De\Idrinth\WalledSecrets\Twig;
 use PDO;
 use phpseclib3\Crypt\AES;
 use phpseclib3\Crypt\Blowfish;
 use phpseclib3\Crypt\Random;
+use Ramsey\Uuid\Uuid;
 
 class Knowns
 {
@@ -17,8 +19,9 @@ class Knowns
     private AES $aes;
     private Blowfish $blowfish;
     private ENV $env;
+    private ShareWithOrganisation $share;
 
-    public function __construct(PDO $database, Twig $twig, AES $aes, Blowfish $blowfish, ENV $env)
+    public function __construct(PDO $database, Twig $twig, AES $aes, Blowfish $blowfish, ENV $env, ShareWithOrganisation $share)
     {
         $this->database = $database;
         $this->twig = $twig;
@@ -31,6 +34,7 @@ class Knowns
         $this->blowfish->setKeyLength(448);
         $this->blowfish->setKey($this->env->getString('PASSWORD_BLOWFISH_KEY'));
         $this->blowfish->setIV($this->env->getString('PASSWORD_BLOWFISH_IV'));
+        $this->share = $share;
     }
 
     public function post(array $post, string $id): string
@@ -54,6 +58,46 @@ WHERE knowns.id=:id AND knowns.`owner`=:account');
         $stmt->execute([':id' => $id, ':account' => $_SESSION['id']]);
         $known = $stmt->fetch(PDO::FETCH_ASSOC);
         if (!$known) {
+            header ('Location: /socials', true, 303);
+            return '';
+        }
+        if (isset($post['identifier']) && isset($post['user']) && isset($post['domain']) && isset($post['password']) && isset($post['note']) && isset($post['identifier'])) {
+            $stmt = $this->database->prepare('SELECT accounts.id,accounts.aid,folders.aid as folder
+FROM accounts
+INNER JOIN knowns ON knowns.target=accounts.aid
+INNER JOIN folders ON knowns.target=folders.`owner` AND folders.`default` AND folders.`type`="Account"
+WHERE knowns.`owner`=:owner AND knowns.id=:id');
+            $stmt->execute([':id' => $id, ':owner' => $_SESSION['id']]);
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
+            $this->share->updateLogin(
+                $data['aid'],
+                $data['id'],
+                $data['folder'],
+                Uuid::uuid1()->toString(),
+                $post['user'],
+                $post['password'],
+                $post['domain'],
+                $post['note'],
+                $post['identifier']
+            );
+            header ('Location: /', true, 303);
+            return '';
+        } elseif (isset($post['content']) && isset($post['name'])) {
+            $stmt = $this->database->prepare('SELECT accounts.id,accounts.aid,folders.aid as folder
+FROM accounts
+INNER JOIN knowns ON knowns.target=accounts.aid
+INNER JOIN folders ON knowns.target=folders.`owner` AND folders.`default` AND folders.`type`="Account"
+WHERE knowns.`owner`=:owner AND knowns.id=:id');
+            $stmt->execute([':id' => $id, ':owner' => $_SESSION['id']]);
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
+            $this->share->updateNote(
+                $data['aid'],
+                $data['id'],
+                $data['folder'],
+                Uuid::uuid1()->toString(),
+                $post['name'],
+                $post['content']
+            );
             header ('Location: /', true, 303);
             return '';
         }
