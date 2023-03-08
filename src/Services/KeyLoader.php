7a1fbf85286543099fd3030330a4814ddb65f26e
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-08 12:53:48 +0100

    prevent emptyp assword

--- src/Services/KeyLoader.php
+++ src/Services/KeyLoader.php
@@ -9,15 +9,18 @@ use phpseclib3\Crypt\RSA;
 
 class KeyLoader
 {
-    public static function private($uuid, $password): PrivateKey
+    public static function private(string $uuid, string $password): PrivateKey
     {
         $file = dirname(__DIR__, 2) . '/keys/' . $uuid . '/private';
         if (!is_file($file)) {
             throw InvalidArgumentException('No private key for user '.$uuid);
         }
+        if (empty($password)) {
+            throw InvalidArgumentException('No password given for user '.$uuid);
+        }
         return RSA::loadPrivateKey(file_get_contents($file), $password);
     }
-    public static function public($uuid): PublicKey
+    public static function public(string $uuid): PublicKey
     {
         $file = dirname(__DIR__, 2) . '/keys/' . $uuid . '/public';
         if (!is_file($file)) {
