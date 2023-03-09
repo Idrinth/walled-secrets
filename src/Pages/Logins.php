78f65fa1997414a99548192038091b7814588fb6
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-09 17:18:09 +0100

    repair note editing allow moving logins to orgs allow moving notes to orgs

--- src/Pages/Logins.php
+++ src/Pages/Logins.php
@@ -75,9 +75,27 @@ class Logins
             header ('Location: /', true, 303);
             return '';
         }
-        if ($folder === 'Organisation') {
+        if (isset($post['organisation']) && !$isOrganisation) {
+            $isOrganisation = true;
+            list($org, $fid) = explode(':', $post['organisation']);
+            $stmt = $this->database->prepare('SELECT aid,`type`,`owner` FROM folders WHERE id=:id');
+            $stmt->execute([':id' => $fid]);
+            $folder = $stmt->fetch(PDO::FETCH_ASSOC);
+            $login['folder'] = $folder['aid'];
+            $stmt = $this->database->prepare('SELECT organisations.aid
+FROM organisations
+INNER JOIN memberships ON memberships.organisation=organisations.aid
+WHERE organisations.id=:id AND memberships.`account`=:USER AND memberships.`role` IN ("Owner"."Administrator","Member")');
+            $stmt->execute([':id' => $org, ':user' => $_SESSION['id']]);
+            $organisation = $stmt->fetchColumn();
+            if (!$organisation || $organisation !== $folder['owner']) {
+                header ('Location: /logins/' . $id, true, 303);
+                return '';
+            }
+        }
+        if ($isOrganisation) {
             $stmt = $this->database->prepare('SELECT `aid`,`id` FROM `memberships` INNER JOIN accounts ON memberships.`account`=accounts.aid WHERE organisation=:org AND `role`<>"Proposed"');
-            $stmt->execute();
+            $stmt->execute([':org' => $folder['owner']]);
             foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $user) {
                 $this->share->updateLogin($user['aid'], $user['id'], $login['folder'], $id, $post['user'], $post['password'], $post['domain'], $post['note']??'', $post['identifier']);
             }
--- src/Pages/Notes.php
+++ src/Pages/Notes.php
@@ -73,17 +73,35 @@ class Notes
             header ('Location: /', true, 303);
             return '';
         }
-        if ($folder === 'Organisation') {
+        if (isset($post['organisation']) && !$isOrganisation) {
+            $isOrganisation = true;
+            list($org, $fid) = explode(':', $post['organisation']);
+            $stmt = $this->database->prepare('SELECT aid,`type`,`owner` FROM folders WHERE id=:id');
+            $stmt->execute([':id' => $fid]);
+            $folder = $stmt->fetch(PDO::FETCH_ASSOC);
+            $login['folder'] = $folder['aid'];
+            $stmt = $this->database->prepare('SELECT organisations.aid
+FROM organisations
+INNER JOIN memberships ON memberships.organisation=organisations.aid
+WHERE organisations.id=:id AND memberships.`account`=:USER AND memberships.`role` IN ("Owner"."Administrator","Member")');
+            $stmt->execute([':id' => $org, ':user' => $_SESSION['id']]);
+            $organisation = $stmt->fetchColumn();
+            if (!$organisation || $organisation !== $folder['owner']) {
+                header ('Location: /notes/' . $id, true, 303);
+                return '';
+            }
+        }
+        if ($isOrganisation) {
             $stmt = $this->database->prepare('SELECT `aid`,`id` FROM `memberships` INNER JOIN accounts ON memberships.`account`=accounts.aid WHERE organisation=:org AND `role`<>"Proposed"');
             $stmt->execute();
             foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $user) {
-                $this->share->updateLogin($user['aid'], $user['id'], $login['folder'], $id, $post['login'], $post['password'], $post['domain'], $post['note'], $post['identifier']);
+                $this->share->updateNote($user['aid'], $user['id'], $login['folder'], $id, $post['name'], $post['content'], $post['identifier']);
             }
-            header ('Location: /', true, 303);
+            header ('Location: /notes/' . $id, true, 303);
             return '';
         }
-        $this->share->updateLogin($_SESSION['id'], $_SESSION['uuid'], $login['folder'], $id, $post['login'], $post['password'], $post['domain'], $post['note'], $post['identifier']);
-        header ('Location: /', true, 303);
+        $this->share->updateNote($_SESSION['id'], $_SESSION['uuid'], $login['folder'], $id, $post['name'], $post['content'], $post['identifier']);
+        header ('Location:  /notes/' . $id, true, 303);
         return '';
     }
 
