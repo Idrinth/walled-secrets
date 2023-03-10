f8baaa0891520c2743b571f8456f88ae8c6eb2cc
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-10 15:09:03 +0100

    add missing use statement

--- src/Pages/Master.php
+++ src/Pages/Master.php
@@ -2,6 +2,7 @@
 
 namespace De\Idrinth\WalledSecrets\Pages;
 
+use De\Idrinth\WalledSecrets\Services\Audit;
 use De\Idrinth\WalledSecrets\Services\ENV;
 use De\Idrinth\WalledSecrets\Services\KeyLoader;
 use De\Idrinth\WalledSecrets\Twig;
