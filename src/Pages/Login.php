bd2da87bf729f072f6f53b7699639cf4b5fce90d
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-12 13:41:28 +0100

    better outputs on login failure

--- src/Pages/Login.php
+++ src/Pages/Login.php
@@ -8,6 +8,7 @@ use De\Idrinth\WalledSecrets\Services\Cookie;
 use De\Idrinth\WalledSecrets\Services\ENV;
 use De\Idrinth\WalledSecrets\Services\KeyLoader;
 use De\Idrinth\WalledSecrets\Services\MasterPassword;
+use De\Idrinth\WalledSecrets\Services\Twig;
 use PDO;
 use Swoole\MySQL\Exception;
 
@@ -17,9 +18,11 @@ class Login
     private ENV $env;
     private MasterPassword $master;
     private Audit $audit;
+    private Twig $twig;
 
-    public function __construct(Audit $audit, PDO $database, ENV $env, MasterPassword $master)
+    public function __construct(Twig $twig, Audit $audit, PDO $database, ENV $env, MasterPassword $master)
     {
+        $this->twig = $twig;
         $this->audit = $audit;
         $this->database = $database;
         $this->env = $env;
@@ -28,30 +31,42 @@ class Login
     public function get(User $user, string $id, string $password): string
     {
         if ($this->master->has()) {
-            header('Location: /', true, 303);
-            return '';
+            return $this->twig->render('login-error', [
+                'title' => 'Login Failed',
+                'error' => 'Master Password could not be retrieved from session. Please try again.'
+            ]);
         }
         $stmt = $this->database->prepare('SELECT aid,since,mail FROM accounts WHERE id=:id AND identifier=:password');
         $stmt->execute([':id' => $id, ':password' => $password]);
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($user) {
-            try {
-                KeyLoader::private($id, $this->master->get());
-            } catch (Exception $ex) {
-                header('Location: /', true, 303);
-                return '';
-            }
-            if (strtotime($user['since']) + $this->env->getInt('SYSTEM_SESSION_DURATION') > time()) {
-                $this->audit->log('signin', 'create', $user['aid'], null, $id);
-                $_SESSION['id'] = $user['aid'];
-                $_SESSION['uuid'] = $id;
-                Cookie::set(
-                    $this->env->getString('SYSTEM_QUICK_LOGIN_COOKIE'),
-                    sha1($this->env->getString('SYSTEM_SALT') . $user['mail']),
-                    $this->env->getInt('SYSTEM_QUICK_LOGIN_DURATION')
-                );
-            }
+        if (!$user) {
+            return $this->twig->render('login-error', [
+                'title' => 'Login Failed',
+                'error' => 'Couldn\'t find a user for the given link.'
+            ]);
         }
+        try {
+            KeyLoader::private($id, $this->master->get());
+        } catch (Exception $ex) {
+            return $this->twig->render('login-error', [
+                'title' => 'Login Failed',
+                'error' => 'Master Password could not be verified, please try again.'
+            ]);
+        }
+        if (strtotime($user['since']) + $this->env->getInt('SYSTEM_SESSION_DURATION') < time()) {
+            return $this->twig->render('login-error', [
+                'title' => 'Login Failed',
+                'error' => 'The given Link is too old, please request a new one.'
+            ]);
+        }
+        $this->audit->log('signin', 'create', $user['aid'], null, $id);
+        $_SESSION['id'] = $user['aid'];
+        $_SESSION['uuid'] = $id;
+        Cookie::set(
+            $this->env->getString('SYSTEM_QUICK_LOGIN_COOKIE'),
+            sha1($this->env->getString('SYSTEM_SALT') . $user['mail']),
+            $this->env->getInt('SYSTEM_QUICK_LOGIN_DURATION')
+        );
         header('Location: /', true, 303);
         return '';
     }
